5e719a51eb7dfcf0da69ccd81b8586c8f2674fce
SND\rkttu_cp <SND\rkttu_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2009-01-27 16:26:11 +0000

    * Compatibility Issue found in Win Vista SP1 / Win Server 2008 with msvcrt module.
    git-svn-id: https://blendxplatform.svn.codeplex.com/svn@22252 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ msvcrt-test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace msvcrt_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            msvcrt.printf("%d %f\r\n", 30, 44.03);
+
+            Console.Read();
+        }
+    }
+}
