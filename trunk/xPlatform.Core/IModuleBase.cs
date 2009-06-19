6151516e9447ac7dd033c0b3a95c485d6225ebef
SND\rkttu_cp <SND\rkttu_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2009-06-19 03:58:18 +0000

    * Core Module Class Relationship Modified
    git-svn-id: https://blendxplatform.svn.codeplex.com/svn@25573 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ trunk/xPlatform.Core/IModuleBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xPlatform
+{
+    public interface IModuleBase
+    {
+        Type BaseType { get; }
+        Type CurrentType { get; }
+    }
+}
--- trunk/xPlatform.Core/ModuleBase.cs
+++ trunk/xPlatform.Core/ModuleBase.cs
@@ -3,13 +3,37 @@
 namespace xPlatform
 {
     [Serializable]
-    public abstract class ModuleBase<T>
-        where T : ModuleBase<T>
+    public abstract class ModuleBase : IModuleBase
+    {
+        public virtual Type BaseType
+        {
+            get { return null; }
+        }
+
+        public virtual Type CurrentType
+        {
+            get { return this.GetType(); }
+        }
+    }
+
+    [Serializable]
+    public abstract class ModuleBase<T> : ModuleBase
+        where T : ModuleBase
     {
         [NonSerialized]
-        public static readonly Type BaseType = typeof(ModuleBase<T>);
+        public static readonly Type ModuleBaseType = typeof(ModuleBase<T>);
 
         [NonSerialized]
-        public static readonly Type CurrentType = typeof(T);
+        public static readonly Type ModuleCurrentType = typeof(T);
+
+        public override Type BaseType
+        {
+            get { return ModuleBaseType; }
+        }
+
+        public override Type CurrentType
+        {
+            get { return ModuleCurrentType; }
+        }
     }
 }
