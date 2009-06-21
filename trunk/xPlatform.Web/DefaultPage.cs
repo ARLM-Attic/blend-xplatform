7b8881d58ee76b5fc939f44d37583523ce1fd6a0
SND\rkttu_cp <SND\rkttu_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2009-06-21 21:06:23 +0000

    * Web project added (TBA)
    git-svn-id: https://blendxplatform.svn.codeplex.com/svn@25609 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ trunk/xPlatform.Web/DefaultPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace xPlatform.Web
+{
+    [Serializable]
+    public partial class DefaultPage : Page
+    {
+        protected Repeater itemRepeater;
+
+        public Repeater ItemRepeater
+        {
+            get { return this.itemRepeater; }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
