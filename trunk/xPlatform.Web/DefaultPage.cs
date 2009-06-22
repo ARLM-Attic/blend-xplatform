e0d252485f5b8111a0f55a87dbc5bbe6fd5af459
SND\rkttu_cp <SND\rkttu_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2009-06-22 06:05:35 +0000

    * xPlatform Web Application Modified
    git-svn-id: https://blendxplatform.svn.codeplex.com/svn@25613 81fbe566-5dc4-48c1-bdea-7421811ca204

--- trunk/xPlatform.Web/DefaultPage.cs
+++ trunk/xPlatform.Web/DefaultPage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web;
+using System.Data;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -8,15 +9,77 @@ namespace xPlatform.Web
     [Serializable]
     public partial class DefaultPage : Page
     {
-        protected Repeater itemRepeater;
+        protected Repeater mainMenu;
 
-        public Repeater ItemRepeater
+        protected virtual DataTable MakeupMainMenu()
         {
-            get { return this.itemRepeater; }
+            DataTable table = new DataTable();
+
+            table.Columns.AddRange(new DataColumn[] {
+                new DataColumn("Index", typeof(int)),
+                new DataColumn("Title", typeof(string)),
+                new DataColumn("Description", typeof(string)),
+                new DataColumn("AssemblyName", typeof(string)),
+            });
+
+            int count = 0;
+
+            foreach (object[] eachRow in new object[][] {
+                new object[] { ++count, "ADVAPI32.DLL", "Microsoft(R) Windows(R) 운영 체제의 ADVAPI32.DLL 모듈 정보를 탐색합니다.", "xPlatform.advapi32" },
+                new object[] { ++count, "COMCTL32.DLL", "Microsoft(R) Windows(R) 운영 체제의 COMCTL32.DLL 모듈 정보를 탐색합니다.", "xPlatform.comctl32" },
+                new object[] { ++count, "GDI32.DLL", "Microsoft(R) Windows(R) 운영 체제의 GDI32.DLL 모듈 정보를 탐색합니다.", "xPlatform.gdi32" },
+                new object[] { ++count, "KERNEL32.DLL", "Microsoft(R) Windows(R) 운영 체제의 KERNEL32.DLL 모듈 정보를 탐색합니다.", "xPlatform.kernel32" },
+                new object[] { ++count, "MSFTEDIT.DLL", "Microsoft(R) Windows(R) 운영 체제의 MSFTEDIT.DLL 모듈 정보를 탐색합니다.", "xPlatform.msftedit" },
+                new object[] { ++count, "MSVCRT.DLL", "Microsoft(R) Visual C++(R) 개발 도구의 MSVCRT.DLL 모듈 정보를 탐색합니다.", "xPlatform.msvcrt" },
+                new object[] { ++count, "PSAPI.DLL", "Microsoft(R) Windows(R) 운영 체제의 PSAPI.DLL 모듈 정보를 탐색합니다.", "xPlatform.psapi" },
+                new object[] { ++count, "SECUR32.DLL", "Microsoft(R) Windows(R) 운영 체제의 SECUR32.DLL 모듈 정보를 탐색합니다.", "xPlatform.secur32" },
+                new object[] { ++count, "USER32.DLL", "Microsoft(R) Windows(R) 운영 체제의 USER32.DLL 모듈 정보를 탐색합니다.", "xPlatform.user32" },
+                new object[] { ++count, "WINSPOOL.DRV", "Microsoft(R) Windows(R) 운영 체제의 WINSPOOL.DRV 모듈 정보를 탐색합니다.", "xPlatform.winspool" },
+            })
+                table.Rows.Add(eachRow);
+
+            return table;
         }
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (this.mainMenu != null)
+            {
+                this.mainMenu.ItemDataBound += new RepeaterItemEventHandler(mainMenu_ItemDataBound);
+                this.mainMenu.DataSource = this.MakeupMainMenu();
+                this.mainMenu.DataBind();
+            }
+        }
+
+        private void mainMenu_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType.Equals(ListItemType.Item) ||
+                e.Item.ItemType.Equals(ListItemType.AlternatingItem))
+            {
+                Label index = e.Item.FindControl("Index") as Label;
+                HyperLink title = e.Item.FindControl("Title") as HyperLink;
+                Label description = e.Item.FindControl("Description") as Label;
+                Label assemblyName = e.Item.FindControl("AssemblyName") as Label;
+                DataRow eachRow = e.Item.DataItem as DataRow;
+
+                if (eachRow == null)
+                    return;
+
+                if (index != null)
+                    index.Text = ((int)eachRow["Index"]).ToString();
+
+                if (title != null)
+                {
+                    title.Text = (string)eachRow["Title"];
+                    title.NavigateUrl = String.Format("~/Detail.aspx?assembly={0}", (string)eachRow["AssemblyName"]);
+                }
+
+                if (description != null)
+                    description.Text = (string)eachRow["Description"];
+
+                if (assemblyName != null)
+                    assemblyName.Text = String.Format("~/Detail.aspx?assembly={0}", (string)eachRow["AssemblyName"]);
+            }
         }
     }
 }
---
+++ trunk/xPlatform.Web/DetailPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace xPlatform.Web
+{
+    [Serializable]
+    public partial class DetailPage : Page
+    {
+        protected Repeater detailMenu;
+    }
+}
