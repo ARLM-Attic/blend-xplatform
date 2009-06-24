b43e32c6315975c9786402e1ed741c267aec7a13
SND\rkttu_cp <SND\rkttu_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2009-06-24 12:35:01 +0000

    * Mersenne Twister Algorithm in progress (http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/VERSIONS/C-LANG/mt19937-64.c)
    git-svn-id: https://blendxplatform.svn.codeplex.com/svn@25662 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ trunk/xPlatform.Math.MT/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xPlatform.Math.MT
+{
+    public class MersenneTwister
+    {
+        protected const int NN = 312;
+        protected const int MM = 156;
+        protected const ulong MATRIX_A = 0xB5026F5AA96619E9uL;
+        protected const ulong UM = 0xFFFFFFFF80000000uL;
+        protected const ulong LM = 0x7FFFFFFFuL;
+    }
+}
