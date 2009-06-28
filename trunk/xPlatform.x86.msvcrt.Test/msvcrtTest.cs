31162b2afc3ee5198265197c9e7b78a8636e191c
SND\rkttu_cp <SND\rkttu_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2009-06-28 21:02:12 +0000

    * msvcrt Test Unit added
    git-svn-id: https://blendxplatform.svn.codeplex.com/svn@25736 81fbe566-5dc4-48c1-bdea-7421811ca204

--- trunk/xPlatform.x86.msvcrt.Test/msvcrtTest.cs
+++ trunk/xPlatform.x86.msvcrt.Test/msvcrtTest.cs
@@ -8,12 +8,6 @@ namespace xPlatform.x86.msvcrt.Test
     [TestFixture]
     public unsafe class msvcrtTest : AssertionHelper
     {
-        static void Main()
-        {
-            msvcrtTest t = new msvcrtTest();
-            t._memccpyTest();
-        }
-
         [Test]
         public void _memccpyTest()
         {
@@ -21,18 +15,53 @@ namespace xPlatform.x86.msvcrt.Test
             GlobalHeapAnsiString buffer = new GlobalHeapAnsiString(61);
             SBytePointer pdest = new SBytePointer();
 
-            msvcrt.printf("Function: _memccpy 60 characters or to character 's'\n");
-            msvcrt.printf("Source: %s\n", __arglist(string1.Address));
+            Console.WriteLine("Function: _memccpy 60 characters or to character 's'\n");
+            Console.WriteLine("Source: {0}\n", string1.ToString());
 
             pdest = msvcrt._memccpy(buffer, string1, 's', 60u);
             pdest.SetData(0);
 
-            msvcrt.printf("Result: %s\n", __arglist(buffer.Address));
-            msvcrt.printf("Length: %d characters\n", __arglist(msvcrt.strlen(buffer.Address)));
+            Console.WriteLine("Result: {0}\n", buffer.ToString());
+            Console.WriteLine("Length: {0} characters\n", msvcrt.strlen(buffer.Address));
+
+            Assert.AreEqual("The quick brown dog jumps over the lazy fox", string1.ToString());
+            Assert.AreEqual("The quick brown dog jumps", buffer.ToString());
+            Assert.AreEqual(25u, (uint)msvcrt.strlen(buffer.Address));
 
             string1.Dispose();
             buffer.Dispose();
             pdest = null;
+        }
+
+        [Test]
+        public void memchrTest()
+        {
+            // TODO: String length property needed.
+            int ch = 'r';
+            GlobalHeapAnsiString str = new GlobalHeapAnsiString("lazy");
+            GlobalHeapAnsiString @string = new GlobalHeapAnsiString("The quick brown dog jumps over the lazy fox");
+            GlobalHeapAnsiString fmt1 = new GlobalHeapAnsiString("         1         2         3         4         5");
+            GlobalHeapAnsiString fmt2 = new GlobalHeapAnsiString("12345678901234567890123456789012345678901234567890");
+
+            SBytePointer pdest;
+            int result;
+
+            Console.WriteLine("String to be searched:\n             {0}\n", @string.ToString());
+            Console.WriteLine("             {0}\n             {1}\n\n", fmt1.ToString(), fmt2.ToString());
+
+            Console.WriteLine("Search char: {0}\n", (char)ch);
+            pdest = msvcrt.memchr(@string, ch, msvcrt.strlen(@string.Address));
+            result = (int)(pdest - @string.Address.ToInt32() + 1);
+
+            if (pdest != null)
+                Console.WriteLine("Result:      {0} found at position {1}\n", (char)ch, result);
+            else
+                Console.WriteLine("Result:      {0} not found\n", (char)ch);
+
+            str.Dispose();
+            @string.Dispose();
+            fmt1.Dispose();
+            fmt2.Dispose();
         }
     }
 }
